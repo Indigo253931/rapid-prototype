182770e9a2618e91c27a466758082b4656ebebbc
Indigo253931 <indigo253931@gmail.com>
2016-08-20 15:32:04 -0600

    additions to strainController.js in client side

---
+++ client/public/scripts/controllers/strainController.js
@@ -0,0 +1,22 @@
+angular.module('MyStrain')
+.controller('StrainsController', StrainsController)
+.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: index.html
+	});
+});
+
+StrainsController.$inject=['$resource'];
+
+function StrainsController($resource){
+	var self = this;
+	this.all = [];
+
+function get(){
+	$resource.get('')
+	.then(function(response){
+		self.all = response.data;
+	});
+}
+}
